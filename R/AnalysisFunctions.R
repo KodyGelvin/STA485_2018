6482492bc48950f2c065ce5a129bfe4d9402342e
Derek Sonderegger <derek.sonderegger@gmail.com>
2018-01-24 13:53:48 -0700

    Adding the pointwise.integration function

---
+++ R/AnalysisFunctions.R
@@ -0,0 +1,17 @@
+#' Calculate Area under the curve when the curve is defined by connect-the-dots
+#' 
+#' @param data a data.frame with columns t and y
+#' @examples 
+#' data <- data.frame( t = 1:10, y = rnorm(10))
+#' pointwise.integrate(data)
+#' @export
+pointwise.integrate <- function(data){
+  
+  f <- function(t, data){
+    out <- approx( data$t, data$y, xout=t ) 
+    return(out$y)
+  }
+  
+  integrate(f, min(data$t), max(data$t), data )    
+  
+}
