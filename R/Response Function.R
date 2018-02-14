3a37ce2e76d75de1c011ff04ea20cd9a24295d53
Derek Sonderegger <derek.sonderegger@gmail.com>
2018-02-14 10:20:33 -0700

    Updating the response function to work with the stepwise response function.

--- R/Response Function.R
+++ R/Response Function.R
@@ -1,31 +1,24 @@
 #' Generate data with varying response functions
-#' @param input, t values with which to create data
-#' @param params, list of parameters which are specified for each differet model
+#' @param input.t, vector of t values with which to create data
+#' @param params, list of parameters which are specified for each different model
+#' @return Returns a vector of response values for the input.t values.
 #' @examples 
 #' 
-#' 
-
-stepwise_params = list(
-    method = 'step',
-    max.height = 2,
-    baseline = 1,
-    duration = 30)
-data <- data.frame( t = -15:60, y = rnorm(76))
-
+#' stepwise_params = list(
+#'    method = 'step',
+#'    max.height = 2,
+#'    baseline = 5,
+#'    duration = 7)
+#'  t <- seq(-2, 10, by=.001)
+#'  y <- response( t, stepwise_params )
+#'  plot(t, y)
 response <- function(input.t, params = list()){
   if(params$method == 'step'){
-    if(data$t > 0){ 
-      ifelse(data$t < 30, 2, 1)     
-    }
-    else{ 
-    y = 1
-    }
+    y <- ifelse(0 < input.t & input.t < params$duration, params$max.height, params$baseline)     
+  }else{
+    error("params$method should be either 'step' or ...")
   }
+  return( y )
 }
-#keeps saying condition has length > 1 and only the first element will be
-# used. It worked before I added the outer layer of conditionals saying
-#conditionals saying that t must be greater than 0 for the pre exercise
-y <- response(data$t, stepwise_params)
-newdata <- data.frame(data$t, y)
-ggplot(data = newdata, aes(y = y, x = data.t)) +
-  geom_line()
+
+
